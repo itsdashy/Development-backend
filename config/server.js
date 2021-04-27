0d55838fc04adccd9547c0ee174a2970622375fb
itsdashy <itsdashy@gmail.com>
2021-04-27 16:44:52 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a19a57423eb8ef5533409a6c290348af'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b79ba387-83b0-4663-a5b1-7daa879f6c27'
+};
